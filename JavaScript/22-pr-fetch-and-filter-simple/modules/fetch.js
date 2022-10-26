c154aca2bee38196ff6e31c413338351dba032f8
tdna <tdna@tdnas-MacBook-Air.local>
2022-10-26 22:06:44 +0300

    Added simple fetch and filter pracctice

---
+++ JavaScript/22-pr-fetch-and-filter-simple/modules/fetch.js
@@ -0,0 +1,22 @@
+// Duomenis pasiimsime iš: https://magnetic-melon-yam.glitch.me
+
+//     Naudojant tik JS, sukurkite lentelę ir į ją įrašykite duomenis (id, name, city, fav_color).
+//     Naudojant JS metodus, padalinkite vardą į dvi dalis: vardą ir pavardę (lentelėje).
+//     Pridėkite prie lentelės (tarp id ir name) nuotrauką.
+//     Sukurkite checkbox virš lentelės su JS. Jį paspaudus, rodys tik tuos žmones, kurie yra VIP.
+//     Sukurkite virš lentelės ir search laukelį (forma su input type search ir mygtukas). Suvedus duomenis, lentelėje turi prasifiltruoti pagal vardą arba pavardę (fullname contains search string). Capitalizacija turėtų būti nesvarbi.
+
+// sukurti VIP užrašą
+
+const getRobots = async () => {
+  try {
+    const response = await fetch("https://magnetic-melon-yam.glitch.me");
+    const robots = await response.json();
+
+    return robots;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getRobots };
