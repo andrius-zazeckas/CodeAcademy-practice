c6399d7583ec632201c121610f6be15056436c18
andriuszaz <a.zazeckas@gmail.com>
2022-11-08 19:02:49 +0200

    Added express js and post practice

---
+++ NodeJs/999-inclass-practice/11_08-POST-express/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = +process.env.PORT || 5001;
+
+app.use(express.json());
+
+app.post("/", (_, res) => {
+  res.send({ message: "Welcome to my project" });
+});
+
+app.listen(PORT, () => console.log(`Server is runing ${PORT}`));
