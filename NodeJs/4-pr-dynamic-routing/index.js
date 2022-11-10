2a30451b9b60300d1a126f0f0afa3cb0772ff5b3
andriuszaz <a.zazeckas@gmail.com>
2022-11-10 21:47:56 +0200

    Added dynamic routing practice

---
+++ NodeJs/4-pr-dynamic-routing/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = 5000;
+
+const cars = {
+  bmw: ["i3", "i8", "1 series", "3 series", "5 series"],
+  mb: ["A class", "C class", "E class", "S class"],
+  vw: ["Golf", "Arteon", "UP"],
+};
+
+app.get("/", (_, res) => {
+  res.send(cars);
+});
+
+app.get("/cars/:model", (req, res) => {
+  const { model } = req.params;
+
+  const car = cars[model];
+
+  res.send(car).end();
+});
+
+app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
