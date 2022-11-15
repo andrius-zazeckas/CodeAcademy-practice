0abf2a021e07cc29cf4a2c0cdb96521f4704b95b
andriuszaz <a.zazeckas@gmail.com>
2022-11-15 21:13:25 +0200

    Added new get with dynamic routing to filter by gender to  show names

--- NodeJs/4-pr-dynamic-routing-and-REST/REST/index.js
+++ NodeJs/4-pr-dynamic-routing-and-REST/REST/index.js
@@ -12,12 +12,12 @@ app.get("/", (_, res) => {
 });
 
 app.get("/email", (_, res) => {
-  function extractValue(arr, prop) {
+  const extractValue = (arr, prop) => {
     // extract value from property
     let extractedValue = arr.map((item) => item[prop]);
 
     return extractedValue;
-  }
+  };
 
   // passing an array of objects and property 'a' to extract
   const result = extractValue(data, "email");
@@ -36,11 +36,32 @@ app.get("/:car", (req, res) => {
 });
 
 app.get("/id/:id", (req, res) => {
-  const { id } = req.params;
+  const id = +req.params.id;
 
-  const user = data.find((curUser) => JSON.stringify(curUser.id) === id);
+  const user = data.find((curUser) => curUser.id === id);
 
   res.send(user).end();
+});
+
+app.get("/gender/:gender", (req, res) => {
+  const { gender } = req.params;
+
+  const doesGenderExists = data.find(
+    (curGender) => curGender.gender.toLowerCase() === gender.toLocaleLowerCase()
+  );
+  if (doesGenderExists) {
+    const filterByGender = data.filter(
+      (curGender) =>
+        curGender.gender.toLowerCase() === gender.toLocaleLowerCase()
+    );
+    const showFilteredNames = filterByGender.map(
+      (curGender) => `${curGender.first_name} ${curGender.last_name}`
+    );
+
+    res.send(showFilteredNames).end();
+    return;
+  }
+  res.status(404).send("Gender does not exist").end();
 });
 
 app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
