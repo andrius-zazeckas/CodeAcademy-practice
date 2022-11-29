f096588fce14302948df87413fecfa4c0f4c0a9e
andriuszaz <a.zazeckas@gmail.com>
2022-11-29 19:57:50 +0200

    Added in class practice with distinct and aggregate

---
+++ NodeJs/999-inclass-practice/11_29-PRACTICE-mongodb-distinct-aggregate/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+
+const app = express();
+require("dotenv").config();
+
+const URI = process.env.URI;
+const client = new MongoClient(URI);
+const PORT = +process.env.PORT || 5001;
+
+const DB = process.env.DB;
+const DBCOLLECTION = process.env.DBCOLLECTION;
+
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const { firstName, hasOrdered } = req.body;
+
+  if (!firstName || typeof firstName !== "string") {
+    res
+      .status(400)
+      .send("First name was not provided or is not a string")
+      .end();
+    return;
+  }
+
+  if (!hasOrdered || typeof hasOrdered !== "boolean") {
+    res.status(400).send("Has ordered is provided incorrectly").end();
+    return;
+  }
+
+  const pipeline = [
+    {
+      $match: {
+        hasOrdered,
+      },
+    },
+    {
+      $group: {
+        _id: "$firstName",
+        totalOrders: { $count: {} },
+      },
+    },
+    {
+      $sort: {
+        totalOrders: -1,
+      },
+    },
+  ];
+
+  try {
+    const docs = [];
+    const con = await client.connect();
+    const db = con.db(DB);
+    const collection = db.collection(DBCOLLECTION);
+
+    const uniqueNames = await collection.distinct("firstName");
+
+    const nameCount = await collection.count({
+      firstName,
+    });
+
+    const aggregationCursor = collection.aggregate(pipeline);
+
+    for await (const doc of aggregationCursor) {
+      docs.push(doc);
+    }
+
+    await con.close();
+
+    res.send({ uniqueNames, nameCount, docs }).end();
+  } catch (error) {
+    res.status(500).send({ error }).end();
+    throw Error(error);
+  }
+});
+
+app.listen(PORT, async () => {
+  console.log(`server is running on ${PORT}`);
+});
