f16e7a3364ff27e4ac4deaba20bb0fcab2643ffb
andriuszaz <a.zazeckas@gmail.com>
2022-12-15 19:09:22 +0200

    Added if statements

--- NodeJs/15-pr-MySQL-CRUD/index.js
+++ NodeJs/15-pr-MySQL-CRUD/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 require("dotenv").config();
 
@@ -35,7 +35,16 @@ app.post("/table", async (_, res) => {
 });
 
 app.get("/items", async (req, res) => {
-  const limit = req.query?.limit;
+  const limit = +req.query?.limit;
+
+  if (Number.isNaN(limit) || limit < 0 || typeof limit !== "number") {
+    return res
+      .status(400)
+      .send({
+        error: `Please provide a proper LIMIT in the URL: current LIMIT ${limit} is incorrect.`,
+      })
+      .end();
+  }
 
   try {
     const con = await mysql.createConnection(MYSQL_CONFIG);
@@ -46,8 +55,61 @@ app.get("/items", async (req, res) => {
 
     res.send(result[0]).end();
   } catch (err) {
-    res.send(err).end();
+    res.status(500).send(err).end();
     return console.error();
+  }
+});
+
+app.post("/items", async (req, res) => {
+  const title = mysql.escape(req.query.title.trim());
+
+  if (!title) {
+    res
+      .status(400)
+      .send("Please input item name in URL to create it in a table.");
+  }
+
+  const query = `INSERT INTO items (title) VALUES(${title})`;
+
+  try {
+    const con = await mysql.createConnection(MYSQL_CONFIG);
+    await con.execute(query);
+
+    await con.end();
+
+    res.send("Data inserted into table").end();
+  } catch (err) {
+    res.send(err).end();
+    return console.err();
+  }
+});
+
+app.delete("/items/:id", async (req, res) => {
+  const id = mysql.escape(req.params.id.trim());
+  const cleanId = +id.replaceAll("'", "");
+
+  if (cleanId < 0 || Number.isNaN(cleanId) || typeof cleanId !== "number") {
+    return res
+      .status(400)
+      .send({
+        error: `Please provide a proper id in the URL: current id ${cleanId} incorrect.`,
+      })
+      .end();
+  }
+
+  try {
+    const con = await mysql.createConnection(MYSQL_CONFIG);
+
+    const query = `DELETE FROM items WHERE id = ${cleanId}`;
+
+    await con.execute(query);
+
+    await con.end();
+
+    res.status(200).send("Item was deleted").end();
+  } catch (err) {
+    res.status(500).send(err).end();
+    return console.error(err);
   }
 });
 
