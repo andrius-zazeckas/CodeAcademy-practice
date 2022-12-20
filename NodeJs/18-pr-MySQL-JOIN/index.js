187d59ee674464ad01007e2885d3c4e09e2628e0
andriuszaz <a.zazeckas@gmail.com>
2022-12-20 20:20:37 +0200

    Added practice with MySQL JOIN

--- NodeJs/18-pr-MySQL-JOIN/index.js
+++ NodeJs/18-pr-MySQL-JOIN/index.js
@@ -70,10 +70,32 @@ app.post("/book", async (req, res) => {
   }
 });
 
+//Pasirašome SQL JOIN, kuris atvaizduotų lentelę visų knygų su jų autoriais (books.id, authors.name, authors.surname, books.title, books.year).
+
 app.get("/books", async (req, res) => {
   try {
     const query =
       "SELECT books.id, authors.firstName, authors.lastName, books.title, books.year FROM books LEFT JOIN authors ON authors.id = books.author_id";
+
+    const con = await mysql.createConnection(MYSQL_CONFIG);
+
+    const result = (await con.execute(query))[0];
+
+    await con.end();
+
+    res.status(200).send(result).end();
+  } catch (err) {
+    res.status(500).send(err).end();
+    return console.error(err);
+  }
+});
+
+//Parašome SQL, kuris atvaizduotų visus autorius ir kiek jie knygų turi (t.y. author.id, author.name, author.surname, count(by books.author_id))
+
+app.get("/books-count", async (req, res) => {
+  try {
+    const query =
+      "SELECT COUNT (books.id) AS bookCount, authors.firstName, authors.lastName FROM books INNER JOIN authors ON authors.id = books.author_id GROUP BY firstName, lastName";
 
     const con = await mysql.createConnection(MYSQL_CONFIG);
 
