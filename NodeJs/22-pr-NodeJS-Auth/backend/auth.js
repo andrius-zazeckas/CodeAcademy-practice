b91e50aa873d0ad186fba3040f640320e4affebf
andriuszaz <a.zazeckas@gmail.com>
2023-01-02 22:44:38 +0200

    Added practice with NodeJS auth, Joi, bcryptjs

---
+++ NodeJs/22-pr-NodeJS-Auth/backend/auth.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const mysql = require("mysql2/promise");
+const Joi = require("joi");
+const bcrypt = require("bcryptjs");
+
+require("./config");
+
+const MYSQL_CONFIG = {
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  port: +process.env.port,
+};
+
+const router = express.Router();
+
+const userSchema = Joi.object({
+  email: Joi.string().email().trim().lowercase().required(),
+  password: Joi.string().required(),
+});
+
+router.post("/register", async (req, res) => {
+  let userData = req.body;
+  try {
+    userData = await userSchema.validateAsync(userData);
+  } catch (error) {
+    return res.status(400).send({ error: "Incorrect data sent" }).end();
+  }
+
+  try {
+    const hashedPassword = bcrypt.hashSync(userData.password);
+
+    const con = await mysql.createConnection(MYSQL_CONFIG);
+    const [data] = await con.execute(
+      `INSERT INTO users (email, password) VALUES (${mysql.escape(
+        userData.email
+      )}, '${hashedPassword}')`
+    );
+
+    await con.end();
+
+    return res.send(data).end();
+  } catch (error) {
+    return res.status(500).send({ error: "Unexpected error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  let userData = req.body;
+  try {
+    userData = await userSchema.validateAsync(userData);
+  } catch (error) {
+    return res.status(400).send({ error: "Incorrect email or password" }).end();
+  }
+
+  try {
+    const con = await mysql.createConnection(MYSQL_CONFIG);
+    const [data] = await con.execute(
+      `SELECT * FROM users WHERE email = ${mysql.escape(userData.email)}`
+    );
+
+    await con.end();
+
+    if (data.length === 0) {
+      return res
+        .status(400)
+        .send({ error: "Incorrect email or password" })
+        .end();
+    }
+
+    const isAuthed = bcrypt.compareSync(userData.password, data[0].password);
+
+    if (isAuthed) {
+      return res.send("OK").end();
+    }
+
+    return res.status(400).send({ error: "Incorrect email or password" }).end();
+  } catch (error) {
+    return res.status(500).send({ error: "Unexpected error" });
+  }
+});
+
+module.exports = router;
