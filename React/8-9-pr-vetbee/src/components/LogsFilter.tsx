0f5c7deef84bbdd2704064023dcca732ac3e55ca
andriuszaz <a.zazeckas@gmail.com>
2023-02-11 21:13:10 +0200

    Refactoring

--- React/8-9-pr-vetbee/src/components/LogsFilter.tsx
+++ React/8-9-pr-vetbee/src/components/LogsFilter.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { ButtonContainer } from "./styles/ButtonContainer";
 import { ColorButton } from "./styles/ColorButton";
 import { FilterContainer } from "./styles/FilterContainer";
+import { TransparentButton } from "./styles/TransparentButton";
 
 export const LogsFilter = ({
   isLogs,
@@ -9,8 +10,19 @@ export const LogsFilter = ({
   isActive,
   setIsActive,
 }: any) => {
+  const [isLogsBtnActive, setIsLogsBtnActive] = useState(true);
+  const [isPrescBtnActive, setIsPrescBtnActive] = useState(true);
+
   const handleClick = () => {
     setIsActive(!isActive);
+  };
+
+  const handleLogBtnActive = () => {
+    setIsLogsBtnActive(!isLogsBtnActive);
+  };
+
+  const handlePrescBtnActive = () => {
+    setIsPrescBtnActive(!isPrescBtnActive);
   };
 
   return (
@@ -24,12 +36,52 @@ export const LogsFilter = ({
             {!isLogs ? (
               <></>
             ) : (
-              <ColorButton onClick={handleClick}>Logs</ColorButton>
+              <>
+                {isLogsBtnActive ? (
+                  <ColorButton
+                    onClick={() => {
+                      handleClick();
+                      handleLogBtnActive();
+                    }}
+                  >
+                    Logs
+                  </ColorButton>
+                ) : (
+                  <TransparentButton
+                    onClick={() => {
+                      handleClick();
+                      handleLogBtnActive();
+                    }}
+                  >
+                    Logs
+                  </TransparentButton>
+                )}
+              </>
             )}
-            {!isPrescriptions ? (
+            {isPrescriptions ? (
               <></>
             ) : (
-              <ColorButton>Prescriptions</ColorButton>
+              <>
+                {isPrescBtnActive ? (
+                  <ColorButton
+                    onClick={() => {
+                      handleClick();
+                      handlePrescBtnActive();
+                    }}
+                  >
+                    Prescriptions
+                  </ColorButton>
+                ) : (
+                  <TransparentButton
+                    onClick={() => {
+                      handleClick();
+                      handlePrescBtnActive();
+                    }}
+                  >
+                    Prescriptions
+                  </TransparentButton>
+                )}
+              </>
             )}
           </ButtonContainer>
         </FilterContainer>
