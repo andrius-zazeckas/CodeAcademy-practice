d06f8f0a7c1180d5af5826a21d20341decd37ff5
andriuszaz <a.zazeckas@gmail.com>
2023-02-14 23:24:05 +0200

    Refactoring

--- React/8-9-pr-vetbee/src/components/DarkModeButton.tsx
+++ React/8-9-pr-vetbee/src/components/DarkModeButton.tsx
@@ -6,6 +6,18 @@ import { TransparentButton } from "./styles/TransparentButton";
 export const DarkModeButton = () => {
   const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
 
+  const changeTheme = () => {
+    if (darkMode) {
+      document.body.style.backgroundColor = "rgb(83, 87, 95)";
+      document.body.style.color = "white";
+    } else {
+      document.body.style.backgroundColor = "";
+      document.body.style.color = "black";
+    }
+  };
+
+  changeTheme();
+
   return (
     <>
       {!darkMode ? (
