da60a94e08ea2449bc1f2ac6d2e2cb64a8e59fa6
andriuszaz <a.zazeckas@gmail.com>
2023-03-09 18:31:01 +0200

    Added cart amount

--- React/999-inclass-practice/ecommerce-clothes-webapp/src/components/index.ts
+++ React/999-inclass-practice/ecommerce-clothes-webapp/src/components/index.ts
@@ -1,5 +1,5 @@
 export { ProductsContext, productsReducer } from "./ProductsContext";
 export { MainRouter } from "./MainRouter";
 export { Products } from "./Products";
-export { Cart } from "./Cart";
 export { NotFoundPage } from "./NotFoundPage";
+export { Cart } from "./Cart";
